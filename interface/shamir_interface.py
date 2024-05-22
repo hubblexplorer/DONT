7fc5840df382f453184c3fc1a8da1577cfa72684
Toze <antonio.caldeira@ubi.pt>
2024-05-22 23:01:04 +0100

    Shamir a funcionar, só falta mudar o status

--- interface/shamir_interface.py
+++ interface/shamir_interface.py
@@ -2,9 +2,9 @@
 # Ao selecionar um, pedir as chaves privadas de cada participante da comissao
 # as chaves tem de estar na pasta, ou tem de ser carregadas para o cache
 # e depois faz a logica para decrypt e depois inicia entao a votaçao
-# era interessante mostrar um cronómetro
 
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 import time
@@ -19,6 +19,7 @@ sys.path.append(parent_dir)
 
 from api.api_db import Database as api
 from shamir import Shamir
+from resources.rsa_sign.keys_rsa import encrypt_message, decrypt_message
 
 class ShamirInterface(tk.Toplevel):
     def __init__(self, master, action, user_id):
@@ -28,11 +29,14 @@ class ShamirInterface(tk.Toplevel):
         self.master = master
         self.user_id = user_id  # Pegue o usuário autenticado do master
         self.action = action
+        self.api_instance = api()
 
         tk.Label(self, text=f"{action} - Shamir's Secret Sharing", font=("Helvetica", 16)).pack(pady=20)
 
-        self.election_list = tk.Listbox(self)
-        self.election_list.pack(pady=10)
+        self.election_tree = ttk.Treeview(self, columns=('ID', 'Nome'), show='headings')
+        self.election_tree.heading('ID', text='ID')
+        self.election_tree.heading('Nome', text='Nome')
+        self.election_tree.pack(pady=10, fill=tk.BOTH, expand=True)
 
         self.load_elections()  # Carregar as eleições do utilizador autenticado
 
@@ -42,38 +46,37 @@ class ShamirInterface(tk.Toplevel):
         self.confirm_button = tk.Button(self, text=f"Confirmar {action}", command=self.confirm_action, state=tk.DISABLED)
         self.confirm_button.pack(pady=10)
 
-        self.timer_label = tk.Label(self, text="00:00:00", font=("Helvetica", 14))
-        self.timer_label.pack(pady=10)
-
-        self.timer_running = False
-
     def load_elections(self):
         # Carregar eleições do utilizador autenticado
-        elections = api.get_user_elections(self.user_id)  # Supondo que você tenha uma função para obter as eleições do usuário
-        for election in elections:
-            self.election_list.insert(tk.END, election['name'])
+        elections = self.api_instance.get_elections_by_user(current_user=self.user_id)
+        for election in elections.value:
+            self.election_tree.insert('', tk.END, values=(election[0], election[1]), tags=(election[5],))
 
     def load_keys(self):
         # Função para carregar chaves privadas
-        selected_election = self.election_list.get(tk.ACTIVE)
-        if not selected_election:
+        selected_item = self.election_tree.selection()
+        if not selected_item:
             messagebox.showwarning("Aviso", "Selecione uma eleição primeiro.")
             return
         
-        self.election_details = api.get_election_details(selected_election)  # Obter detalhes da eleição, incluindo o número de membros da comissão
-        self.required_keys = self.election_details['num_comission_members']
+        self.selected_election_id = self.election_tree.item(selected_item)['values'][0]
+        self.selected_election_comission = self.election_tree.item(selected_item, 'tags')[0]
+
+        self.commission_details = self.api_instance.get_commission_members(current_user=self.user_id, Commission_id=self.selected_election_comission)  # Obter detalhes da eleição, incluindo o número de membros da comissão
+        self.required_keys = len(self.commission_details.value)
         self.keys = filedialog.askopenfilenames(title=f"Selecione as {self.required_keys} chaves privadas", filetypes=(("Key files", "*.PEM"), ("All files", "*.*")))
         
         if len(self.keys) == self.required_keys:
             messagebox.showinfo("Chaves Carregadas", f"{len(self.keys)} chaves carregadas com sucesso.")
             self.confirm_button.config(state=tk.NORMAL)
+            print(self.keys)
         else:
             messagebox.showwarning("Aviso", f"Precisam ser carregadas exatamente {self.required_keys} chaves.")
 
     def confirm_action(self):
         # Função para confirmar a ação (iniciar ou fechar votação)
-        selected_election = self.election_list.get(tk.ACTIVE)
-        if selected_election:
+        selected_item = self.election_tree.selection()
+        if selected_item:
             if self.verify_keys():
                 if self.action == "Iniciar Votação":
                     self.start_voting()
@@ -84,29 +87,35 @@ class ShamirInterface(tk.Toplevel):
 
     def verify_keys(self):
         # Verificar as chaves usando Shamir's Secret Sharing
-        return Shamir.reconstruct_init(self.keys)  # Supondo que você tenha uma função para verificar as chaves
+        try:
+            keys_parts = []
+            for i, member in enumerate(self.commission_details.value):
+                key = member[2]
+                with open(self.keys[i], 'r') as file:
+                    private_key = file.read()
+                decrypted_key, prime = decrypt_message(private_key, key).split('|DIV|')
+                x, y = map(int, decrypted_key.strip('()').split(','))
+                keys_parts.append((x, y))
+            
+            # Reconstruir o segredo usando Shamir's Secret Sharing
+            shamir = Shamir(keys_parts, int(prime))
+            secret = shamir.secret
+            
+            # Verifique se o segredo foi reconstruído corretamente
+            if secret:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f"Erro ao verificar as chaves: {e}")
+            return False
 
     def start_voting(self):
         # Lógica para iniciar a votação
+        self.api_instance.change_election_active_status(current_user=self.user_id, Id_election=self.selected_election_id, status=True)
         messagebox.showinfo("Iniciar Votação", "Votação iniciada com sucesso.")
-        self.start_timer()
 
     def close_voting(self):
         # Lógica para fechar a votação
+        self.api_instance.change_election_active_status(current_user=self.user_id, Id_election=self.selected_election_id, status=False)
         messagebox.showinfo("Fechar Votação", "Votação fechada com sucesso.")
-        self.stop_timer()
-
-    def start_timer(self):
-        self.timer_running = True
-        self.start_time = time.time()
-        self.update_timer()
-
-    def stop_timer(self):
-        self.timer_running = False
-
-    def update_timer(self):
-        if self.timer_running:
-            elapsed_time = time.time() - self.start_time
-            formatted_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
-            self.timer_label.config(text=formatted_time)
-            self.after(1000, self.update_timer)
